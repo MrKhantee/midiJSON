f8724c6c9b8e50b9b7c9e9b76f34c6a81d71ca2b
Joseph Weidinger <josephweidinger@carfax.com>
2017-03-28 18:34:46 -0500

    basic file read

--- src/main.js
+++ src/main.js
@@ -1,2 +1,32 @@
 const fs = require('fs');
-const file = fs.readFile('../sample.mid')
+const path = require('path');
+
+import { parseMidiFile } from './parser'
+
+const pathToSampleMid = __dirname + '/sampleMidiFiles/sample2_bach.mid'
+
+const isMidiFile = (firstFourBytes) => {
+    return firstFourBytes.toString() === "MThd"
+}
+
+const file = fs.readFile(pathToSampleMid, (err, data) => {
+
+    if (err) {
+
+        throw err
+
+    }
+
+    const firstFourBytes = data.slice(0, 4)
+
+    if (!isMidiFile(firstFourBytes)) {
+
+        throw new Error("File is not a MIDI file!")
+
+    } else {
+
+        parseMidiFile(data)
+
+    }
+
+})
---
+++ src/parser.js
@@ -0,0 +1,7 @@
+export const parseMidiFile = (data) => {
+
+    for (let i = 0; i < 10; i++) {
+        console.log(data[i])
+    }
+
+}
